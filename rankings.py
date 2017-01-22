11e1a342c4d30be77f63628031d18dc8f986e86f
gitronald <gitronald@users.noreply.github.com>
2017-01-22 15:33:34 -0500

    Improve algorithms, add documentation

--- rankings.py
+++ rankings.py
@@ -1,18 +1,56 @@
+# coding=utf8
+"""
+Measures for comparing ranked lists.
+"""
 
-import string
-import random
-from collections import Counter
+from __future__ import division
+from string import ascii_letters, digits
 from jellyfish import damerau_levenshtein_distance
+from numpy import random
+__author__ = """Ronald E Robertson (robertson.ron@husky.neu.edu)"""
+__all__ = ['alphanumerics',
+           'random_string',
+           'generate_alphanum_dict',
+           'rank_as_string',
+           'compare_ranks']
 
 def alphanumerics():
     """Generate all upper and lowercase letters and digits"""
-    return string.ascii_letters + string.digits
+    return ascii_letters + digits
 
 def random_string(length, replacement = False, selection = None):
     """Generate a random string of alphanumerics"""
     selection = list(alphanumerics()) if selection is None else selection
-    return ''.join(np.random.choice(selection, length, replace=replacement))
+    return ''.join(random.choice(selection, length, replace=replacement))
 
+def generate_alphanum_dict(list1):
+    """Generate an alphanumeric dictionary
+    
+    Creates an alphanumeric dictionary where they keys are the
+    inputted list items and the values are selected from a random
+    alphanumeric string generated with no replacement.
+    """
+    chars = random_string(len(list1))   
+    alphanum_dict = {item: chars[i] for i, item in enumerate(list1)}
+    return alphanum_dict
+
+def rank_as_string(list1, alphanum_index):
+    """
+    Convert a ranked list of items into a string of characters 
+    based on a given dictionary `alph` of the format that contains 
+    the ranked items and a random alphanumeric to represent it.
+    
+    Parameters
+    ----------
+        list1 : list
+            A list of rankings to be converted to characters
+            
+        alph : dict
+            A dictionary containing the items in list1 as keys
+            and unique characters as the values.
+    """
+    return ''.join([alphanum_index[l] for l in list1])
+    
 def compare_ranks(list1, list2):
     """Compare two ranked lists
 
@@ -27,24 +65,23 @@ def compare_ranks(list1, list2):
     Returns
     -------
     (j, dist) : tuple
-       j : Jaccard Index - the size of the intersection over the size of the union of 
-       		 two lists. Provides a similarity measure of two lists without regard to order.
-			 dist : the Damerau-Levenshtein edit distance [1] between two lists. The number 
-			 		 list items that must be deleted, inserted, substituted, or swapped to make
-			 		 the two lists identical
-       measured as the percent overlap in list items. Edit disctance is measured
-       as the number of list item swaps would be required for the two lists to be 
-       identical
+       j : Jaccard Index - the size of the intersection over the size of 
+           the union of two lists. Provides a similarity measure of two 
+           lists without regard to order.
+			 dist : Damerau-Levenshtein edit distance [1] - the number of list 
+           items that must be deleted, inserted, substituted, or swapped 
+           to make the rankings of the two lists identical.
 
     Examples
     --------
-    >>> list1 = ['sam', 'sue', 'stew', 'baloo']
-    >>> list2 = ['sue', 'sam', 'baloo', 'new']
+    >>> list1 = ['sam', 'sue', 'stew', 'baloo', 'baloo']
+    >>> list2 = ['sue', 'sam', 'baloo', 'new', 'baloo']
     >>> compare_ranks(list1, list2)
+    (0.0, 3)
 
     Notes
     -----
-    This calls jellyfish.damerau_levenshtein_distance
+    This function uses jellyfish.damerau_levenshtein_distance
 
     References
     ----------
@@ -52,46 +89,20 @@ def compare_ranks(list1, list2):
     			 of spelling errors. CACM, 7(3), 1964.
 
     """
-
-    c1 = Counter(list1)
-    c2 = Counter(list2)
-    c_all = c1 | c2
     
-    overlap = len([x for x in c1 if x in c2])
-    total = len(c1) if len(c1) > len(c2) else len(c2)
-    j = overlap / total
+    intersection = set(list1).intersection(set(list2))
+    union = set(list1).union(set(list2))
+    j = len(intersection) / len(union)
     j = round(j, 3)
-    
-    alph = {}
-    chars = random_string(len(c_all))
-    for i, item in enumerate(c_all):
-        print chars[i], item
-        alph[item] = chars[i]
 
-    alph = {item: char for char in chars}    
-    
-    str1 = rank_as_string(list1, alph)
-    str2 = rank_as_string(list2, alph)
-    
+    alphanum_dict = generate_alphanum_dict(union)  
+    str1 = rank_as_string(list1, alphanum_dict)
+    str2 = rank_as_string(list2, alphanum_dict)
     dist = damerau_levenshtein_distance(unicode(str1), unicode(str2))
 
-    print("list1: ", str1, "len: ", len(str1))
-    print("list2: ", str2, "len: ", len(str2))
+    print "list1:  %s    len: %d" % (str1, len(str1))
+    print "list1:  %s    len: %d" % (str2, len(str2))
     return (j, dist)
 
-def rank_as_string(list1, alph):
-    '''
-    Convert a ranked list of items into a string of characters 
-    based on a given dictionary `alph` of the format that contains 
-    the ranked items and a random alphanumeric to represent it. 
-    (e.g. alph[list1[0]] = 'k', alph[list1[1]] = '4')
-    
-    
-    Args:
-        urls1 (:list: str) A list of unique rankings to be converted to letters
-        alph (str) An alphabet 
-    '''
-    string = ''
-    for r in list1:
-        string += alph[r]
-    return string
+
+
